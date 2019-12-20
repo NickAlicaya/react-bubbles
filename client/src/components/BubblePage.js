a6c5d817d1801aa193fcb0769a200e1f5f185951
nicanor-alicaya <donalicaya@gmail.com>
2019-12-20 13:51:14 -0500

    MVP, login page needs styling :(

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,18 +1,32 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
-const BubblePage = () => {
+const BubblePage = (props) => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+  const logout = e => {
+    localStorage.removeItem("token");
+    props.history.push("/");
+  };
+
+  useEffect(() => {
+    axiosWithAuth().get("http://localhost:5000/api/colors")
+    .then((res) => {
+      console.log("BUBBLEPAGE .GET",res);
+      setColorList(res.data);
+    })
+    .catch(err => console.log("SORRY NO COLORS",err))
+  },[])
 
   return (
     <>
       <ColorList colors={colorList} updateColors={setColorList} />
       <Bubbles colors={colorList} />
+      <button onClick={logout}>Logout</button>
     </>
   );
 };
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,57 @@
-import React from "react";
+import React, {useState} from "react";
+import {axiosWithAuth} from "../utils/axiosWithAuth";
 
-const Login = () => {
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [login, setLogin] = useState(
+    {
+     username: "",
+     password: ""
+   }
+ );
+
+ const handleChange = e => {
+  setLogin({
+      ...login,
+      [e.target.name]: e.target.value
+  });
+  console.log(login)
+};
+
+// (username === "Lambda School" && password === "i<3Lambd4")
+const handleSubmit = e => {
+  e.preventDefault();
+  axiosWithAuth()
+    .post("/login", login)
+    .then(res => {
+      console.log(res);
+      window.localStorage.setItem("token", res.data.payload);
+      props.history.push("/bubbles");
+    })
+    .catch(err =>console.log("LOGIN ERROR",err));
+};
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <div className="mainCont">
+      <h1 className="welcomeText">Welcome to the Bubble App!</h1>
+      <form onSubmit={handleSubmit}>
+          <input
+              type="text"
+              name="username"
+              placeholder="Enter Username"
+              value={login.username}
+              onChange={handleChange}
+            />
+          <input
+              type="password"
+              name="password"
+              placeholder="Enter Password"
+              value={login.password}
+              onChange={handleChange}
+            />
+          <button>Log in</button>
+        </form>
+    </div>
   );
 };
 
